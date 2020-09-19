bbc97ba7714f7f599fdb520f6ccb58c6aeb53b96
Voinov Nikita <nikita-nikita2012@mail.ru>
2020-09-19 19:28:35 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ],
+            },
+        ],
+    },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'disr'),
+        filename: 'app.bundle.js',
+    },
+};
+const HtmlWebpackPlugin =require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+plugins: [
+    new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin ({
+        filename: 'style.css'
+    })
+]
